fb9f2527a3f49540c55d59f08716d468cdd6993f
TheMalachite <eliasgheeraert@gmail.com>
2021-05-05 22:35:33 +0200

    drivers: mtk-base: power: Globally silence MT6785 power logspam in dmesg
    - We don't need to know any of those if we don't debug kernel and it's responsible of bad jitter result

--- drivers/misc/mediatek/base/power/clkbuf_v1/mtk_clkbuf_ctl.c
+++ drivers/misc/mediatek/base/power/clkbuf_v1/mtk_clkbuf_ctl.c
@@ -51,7 +51,7 @@ void __attribute__((weak)) clk_buf_dump_clkbuf_log(void)
 
 int __attribute__((weak)) clk_buf_fs_init(void)
 {
-	pr_info("%s: dummy func\n", __func__);
+	pr_debug("%s: dummy func\n", __func__);
 	return 0;
 }
 
