8c55042cd14d6fecd1d94cf4f1a6f8be8f7764ce
cxzx75 <153367621+cxzx75@users.noreply.github.com>
2026-08-16 14:17:35 +0200

    sound/soc/mediatek/mt6785: Silence logspam

--- sound/soc/mediatek/mt6785/mt6785-afe-pcm.c
+++ sound/soc/mediatek/mt6785/mt6785-afe-pcm.c
@@ -135,7 +135,7 @@ int mt6785_fe_trigger(struct snd_pcm_substream *substream, int cmd,
 	int fs;
 	int ret = 0;
 
-	dev_info(afe->dev, "%s(), %s cmd %d, irq_id %d\n",
+	dev_dbg(afe->dev, "%s(), %s cmd %d, irq_id %d\n",
 		 __func__, memif->data->name, cmd, irq_id);
 
 	switch (cmd) {
@@ -653,7 +653,7 @@ static int mt6785_irq_cnt1_set(struct snd_kcontrol *kcontrol,
 	int irq_id = memif->irq_usage;
 	int irq_cnt = afe_priv->irq_cnt[memif_num];
 
-	dev_info(afe->dev, "%s(), irq_id %d, irq_cnt = %d, value = %ld\n",
+	dev_dbg(afe->dev, "%s(), irq_id %d, irq_cnt = %d, value = %ld\n",
 		 __func__,
 		 irq_id, irq_cnt,
 		 ucontrol->value.integer.value[0]);
@@ -665,7 +665,7 @@ static int mt6785_irq_cnt1_set(struct snd_kcontrol *kcontrol,
 	afe_priv->irq_cnt[memif_num] = irq_cnt;
 
 	if (pm_runtime_status_suspended(afe->dev) || irq_id < 0) {
-		dev_info(afe->dev, "%s(), suspended || irq_id %d, not set\n",
+		dev_dbg(afe->dev, "%s(), suspended || irq_id %d, not set\n",
 			 __func__, irq_id);
 	} else {
 		struct mtk_base_afe_irq *irqs = &afe->irqs[irq_id];
@@ -703,7 +703,7 @@ static int mt6785_irq_cnt2_set(struct snd_kcontrol *kcontrol,
 	int irq_id = memif->irq_usage;
 	int irq_cnt = afe_priv->irq_cnt[memif_num];
 
-	dev_info(afe->dev, "%s(), irq_id %d, irq_cnt = %d, value = %ld\n",
+	dev_dbg(afe->dev, "%s(), irq_id %d, irq_cnt = %d, value = %ld\n",
 		 __func__,
 		 irq_id, irq_cnt,
 		 ucontrol->value.integer.value[0]);
@@ -715,7 +715,7 @@ static int mt6785_irq_cnt2_set(struct snd_kcontrol *kcontrol,
 	afe_priv->irq_cnt[memif_num] = irq_cnt;
 
 	if (pm_runtime_status_suspended(afe->dev) || irq_id < 0) {
-		dev_info(afe->dev, "%s(), suspended || irq_id %d, not set\n",
+		dev_dbg(afe->dev, "%s(), suspended || irq_id %d, not set\n",
 			 __func__, irq_id);
 	} else {
 		struct mtk_base_afe_irq *irqs = &afe->irqs[irq_id];
@@ -752,7 +752,7 @@ static int mt6785_deep_irq_cnt_set(struct snd_kcontrol *kcontrol,
 	int irq_id = memif->irq_usage;
 	int irq_cnt = afe_priv->irq_cnt[memif_num];
 
-	dev_info(afe->dev, "%s(), irq_id %d, irq_cnt = %d, value = %ld\n",
+	dev_dbg(afe->dev, "%s(), irq_id %d, irq_cnt = %d, value = %ld\n",
 		 __func__,
 		 irq_id, irq_cnt,
 		 ucontrol->value.integer.value[0]);
@@ -764,7 +764,7 @@ static int mt6785_deep_irq_cnt_set(struct snd_kcontrol *kcontrol,
 	afe_priv->irq_cnt[memif_num] = irq_cnt;
 
 	if (pm_runtime_status_suspended(afe->dev) || irq_id < 0) {
-		dev_info(afe->dev, "%s(), suspended || irq_id %d, not set\n",
+		dev_dbg(afe->dev, "%s(), suspended || irq_id %d, not set\n",
 			 __func__, irq_id);
 	} else {
 		struct mtk_base_afe_irq *irqs = &afe->irqs[irq_id];
@@ -801,7 +801,7 @@ static int mt6785_voip_rx_irq_cnt_set(struct snd_kcontrol *kcontrol,
 	int irq_id = memif->irq_usage;
 	int irq_cnt = afe_priv->irq_cnt[memif_num];
 
-	dev_info(afe->dev, "%s(), irq_id %d, irq_cnt = %d, value = %ld\n",
+	dev_dbg(afe->dev, "%s(), irq_id %d, irq_cnt = %d, value = %ld\n",
 		 __func__,
 		 irq_id, irq_cnt,
 		 ucontrol->value.integer.value[0]);
@@ -813,7 +813,7 @@ static int mt6785_voip_rx_irq_cnt_set(struct snd_kcontrol *kcontrol,
 	afe_priv->irq_cnt[memif_num] = irq_cnt;
 
 	if (pm_runtime_status_suspended(afe->dev) || irq_id < 0) {
-		dev_info(afe->dev, "%s(), suspended || irq_id %d, not set\n",
+		dev_dbg(afe->dev, "%s(), suspended || irq_id %d, not set\n",
 			 __func__, irq_id);
 	} else {
 		struct mtk_base_afe_irq *irqs = &afe->irqs[irq_id];
@@ -975,7 +975,7 @@ static int mt6785_record_xrun_assert_set(struct snd_kcontrol *kcontrol,
 	struct mt6785_afe_private *afe_priv = afe->platform_priv;
 	int xrun_assert = ucontrol->value.integer.value[0];
 
-	dev_info(afe->dev, "%s(), xrun_assert %d\n", __func__, xrun_assert);
+	dev_dbg(afe->dev, "%s(), xrun_assert %d\n", __func__, xrun_assert);
 	afe_priv->xrun_assert[MT6785_RECORD_MEMIF] = xrun_assert;
 	return 0;
 }
@@ -1000,7 +1000,7 @@ static int mt6785_echo_ref_xrun_assert_set(struct snd_kcontrol *kcontrol,
 	struct mt6785_afe_private *afe_priv = afe->platform_priv;
 	int xrun_assert = ucontrol->value.integer.value[0];
 
-	dev_info(afe->dev, "%s(), xrun_assert %d\n", __func__, xrun_assert);
+	dev_dbg(afe->dev, "%s(), xrun_assert %d\n", __func__, xrun_assert);
 	afe_priv->xrun_assert[MT6785_ECHO_REF_MEMIF] = xrun_assert;
 	return 0;
 }
@@ -1092,7 +1092,7 @@ static int mt6785_adsp_mem_get(struct snd_kcontrol *kcontrol,
 					      ADSP_TASK_ATTR_MEMUL);
 		break;
 	default:
-		pr_info("%s(), task_id %d do not use shared mem\n",
+		pr_debug("%s(), task_id %d do not use shared mem\n",
 			__func__, task_id);
 		break;
 	};
@@ -1140,7 +1140,7 @@ static int mt6785_adsp_mem_set(struct snd_kcontrol *kcontrol,
 						  ADSP_TASK_ATTR_MEMREF);
 		break;
 	default:
-		pr_info("%s(), task_id %d do not use shared mem\n",
+		pr_debug("%s(), task_id %d do not use shared mem\n",
 			__func__, task_id);
 		break;
 	};
@@ -1197,7 +1197,7 @@ static int mt6785_mmap_dl_scene_set(struct snd_kcontrol *kcontrol,
 	} else
 		memif->use_mmap_share_mem = 0;
 
-	dev_info(afe->dev, "%s(), state %d, mem %d\n", __func__,
+	dev_dbg(afe->dev, "%s(), state %d, mem %d\n", __func__,
 		 afe_priv->mmap_playback_state, memif->use_mmap_share_mem);
 	return 0;
 }
@@ -1235,7 +1235,7 @@ static int mt6785_mmap_ul_scene_set(struct snd_kcontrol *kcontrol,
 	} else
 		memif->use_mmap_share_mem = 0;
 
-	dev_info(afe->dev, "%s(), state %d, mem %d\n", __func__,
+	dev_dbg(afe->dev, "%s(), state %d, mem %d\n", __func__,
 		 afe_priv->mmap_record_state, memif->use_mmap_share_mem);
 	return 0;
 }
@@ -1264,7 +1264,7 @@ static int mt6785_dl_mmap_fd_get(struct snd_kcontrol *kcontrol,
 
 	ucontrol->value.integer.value[0] = (memif->use_mmap_share_mem == 1) ?
 					    mtk_get_mmap_dl_fd() : 0;
-	dev_info(afe->dev, "%s, fd %ld\n", __func__,
+	dev_dbg(afe->dev, "%s, fd %ld\n", __func__,
 		 ucontrol->value.integer.value[0]);
 	return 0;
 }
@@ -1285,7 +1285,7 @@ static int mt6785_ul_mmap_fd_get(struct snd_kcontrol *kcontrol,
 
 	ucontrol->value.integer.value[0] = (memif->use_mmap_share_mem == 2) ?
 					    mtk_get_mmap_ul_fd() : 0;
-	dev_info(afe->dev, "%s, fd %ld\n", __func__,
+	dev_dbg(afe->dev, "%s, fd %ld\n", __func__,
 		 ucontrol->value.integer.value[0]);
 	return 0;
 }
@@ -5441,7 +5441,7 @@ static int mt6785_afe_pcm_dev_probe(struct platform_device *pdev)
 		}
 	}
 
-	dev_info(dev, "%s(), mtk_afe_combine_sub_dai\n", __func__);
+	dev_dbg(dev, "%s(), mtk_afe_combine_sub_dai\n", __func__);
 
 	/* init dai_driver and component_driver */
 	ret = mtk_afe_combine_sub_dai(afe);
@@ -5471,7 +5471,7 @@ static int mt6785_afe_pcm_dev_probe(struct platform_device *pdev)
 					   S_IFREG | 0444, NULL,
 					   afe, &mt6785_debugfs_ops);
 #endif
-	dev_info(dev, "%s(), devm_snd_soc_register_platform\n", __func__);
+	dev_dbg(dev, "%s(), devm_snd_soc_register_platform\n", __func__);
 	/* register platform */
 	ret = devm_snd_soc_register_component(&pdev->dev,
 					     &mt6785_afe_component, NULL, 0);
@@ -5480,7 +5480,7 @@ static int mt6785_afe_pcm_dev_probe(struct platform_device *pdev)
 		goto err_platform;
 	}
 
-	dev_info(dev, "%s(), devm_snd_soc_register_component\n", __func__);
+	dev_dbg(dev, "%s(), devm_snd_soc_register_component\n", __func__);
 
 	ret = devm_snd_soc_register_component(&pdev->dev,
 					      &mt6785_afe_pcm_component,
@@ -5491,7 +5491,7 @@ static int mt6785_afe_pcm_dev_probe(struct platform_device *pdev)
 		goto err_dai_component;
 	}
 
-	dev_info(dev, "%s(), --\n", __func__);
+	dev_dbg(dev, "%s(), --\n", __func__);
 
 #if defined(CONFIG_SND_SOC_MTK_AUDIO_DSP) ||\
 	defined(CONFIG_SND_SOC_MTK_SCP_SMARTPA)
@@ -5538,7 +5538,7 @@ static int mt6785_afe_suspend(struct device *dev)
 {
 	struct mtk_base_afe *afe = dev_get_drvdata(dev);
 
-	dev_info(afe->dev, "%s()\n", __func__);
+	dev_dbg(afe->dev, "%s()\n", __func__);
 
 	mt6785_afe_suspend_clock(afe);
 
@@ -5549,7 +5549,7 @@ static int mt6785_afe_resume(struct device *dev)
 {
 	struct mtk_base_afe *afe = dev_get_drvdata(dev);
 
-	dev_info(afe->dev, "%s()\n", __func__);
+	dev_dbg(afe->dev, "%s()\n", __func__);
 
 	mt6785_afe_resume_clock(afe);
 
